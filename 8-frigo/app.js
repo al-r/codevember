d5680119c1a2a430952d28f37e37b3dfcb8cc8a1
alribo <30950535+alribo@users.noreply.github.com>
2017-11-08 21:26:23 +0000

    Add animation and improve tree

--- 8-frigo/app.js
+++ 8-frigo/app.js
@@ -1,5 +1,20 @@
-var windowH = window.innerHeight,
-	windowW = window.innerWidth;
-	
+function displayFlakes(){
+	var nbFlakes = 20,
+		spaceW = 5;
+
+	for(var index=0; index < nbFlakes; index++){
+		var flake = document.createElement("div");
+		flake.className += "flake";
+		var where = document.getElementById("shelves");
+		where.appendChild(flake);
+
+		var	posLeft = spaceW * (index+1);
+		flake.style.left += posLeft + 'px';
+		flake.style.animationDuration = 1 + Math.round(Math.random() * 20) + "s";
+		flake.style.animationDelay = 1 + Math.round(Math.random() * 10) + "s";
+	};
+};
+
 window.addEventListener('load', function(){
+	displayFlakes();
 });
