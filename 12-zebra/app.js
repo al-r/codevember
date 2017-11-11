ee886361ae6b1a23f9e200988e9cbeff6dce3fde
alribo <30950535+alribo@users.noreply.github.com>
2017-11-11 23:49:46 +0000

    Add ground for zebra

--- 12-zebra/app.js
+++ 12-zebra/app.js
@@ -20,7 +20,6 @@ scene.add(pointLight);
 /* ***** ZEBRA ***** */
 var zebra;
 zebra = new THREE.Group();
-zebra.name = "zebra";
 scene.add(zebra);
 
 var blackMaterial = new THREE.MeshLambertMaterial({
@@ -28,7 +27,9 @@ var blackMaterial = new THREE.MeshLambertMaterial({
 });
 /*Picture from Pixabay (https://cdn.pixabay.com/photo/2016/04/08/00/41/seamless-1315322_960_720.jpg)*/
 var texture = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/al-r/codevember/master/12-zebra/zebra.jpg' );
+var textureH = new THREE.TextureLoader().load( 'https://raw.githubusercontent.com/al-r/codevember/master/12-zebra/zebra-h.jpg' );
 var material = new THREE.MeshBasicMaterial( { map: texture } );
+var materialH = new THREE.MeshBasicMaterial( { map: textureH } );
 
 //body
 var geometry = new THREE.BoxGeometry(100, 70, 150);
@@ -37,7 +38,7 @@ zebra.add(cube);
 
 //leg front right
 var legFRGeometry = new THREE.BoxGeometry(20, 150, 20);
-var legFR = new THREE.Mesh(legFRGeometry, material);
+var legFR = new THREE.Mesh(legFRGeometry, materialH);
 legFR.position.set(50,-50,65);
 zebra.add(legFR);
 //Feet front right
@@ -48,7 +49,7 @@ zebra.add(feetFR);
 
 //leg front left
 var legFLGeometry = new THREE.BoxGeometry(20, 150, 20);
-var legFL = new THREE.Mesh(legFLGeometry, material);
+var legFL = new THREE.Mesh(legFLGeometry, materialH);
 legFL.position.set(-50,-50,65);
 zebra.add(legFL);
 //Feet front left
@@ -59,7 +60,7 @@ zebra.add(feetFL);
 
 //leg back right
 var legBRGeometry = new THREE.BoxGeometry(20, 150, 20);
-var legBR = new THREE.Mesh(legBRGeometry, material);
+var legBR = new THREE.Mesh(legBRGeometry, materialH);
 legBR.position.set(50,-50,-65);
 zebra.add(legBR);
 //feet back right
@@ -70,7 +71,7 @@ zebra.add(feetBR);
 
 //leg back left
 var legBLGeometry = new THREE.BoxGeometry(20, 150, 20);
-var legBL = new THREE.Mesh(legBLGeometry, material);
+var legBL = new THREE.Mesh(legBLGeometry, materialH);
 legBL.position.set(-50,-50,-65);
 zebra.add(legBL);
 //feet back left
@@ -81,7 +82,7 @@ zebra.add(feetBL);
 
 //Tail
 var tailGeometry = new THREE.BoxGeometry(10, 60, 10);
-var tail = new THREE.Mesh(tailGeometry, material);
+var tail = new THREE.Mesh(tailGeometry, materialH);
 tail.position.set(0,-25,-80);
 zebra.add(tail);
 
@@ -129,7 +130,7 @@ head.add(mouthBottom);
 
 //Neck
 var neckGeometry = new THREE.BoxGeometry(40, 80, 20);
-var neck = new THREE.Mesh(neckGeometry, material);
+var neck = new THREE.Mesh(neckGeometry, materialH);
 neck.position.set(0,50,65);
 head.add(neck);
 
@@ -148,25 +149,67 @@ head.rotation.x = -100;
 head.position.set(0,54,0);
 
 
-zebra.rotation.y = 20;
+zebra.rotation.y = 0;
+
+
+
+/* ***** GRASS ***** */
+var grass = new THREE.Group();
+zebra.add(grass);
+
+//Grass
+var greenGeometry = new THREE.CylinderGeometry( 200, 210, 5, 20 );
+var greenMaterial = new THREE.MeshLambertMaterial( {color: 0x319c27} );
+var green = new THREE.Mesh( greenGeometry, greenMaterial );
+green.position.set(0,-135,0);
+grass.add(green );
+
+//Blades of grass
+var geometry = new THREE.BoxGeometry(5, 50, 5);
+var blade = new THREE.Mesh(geometry, greenMaterial);
+blade.position.set(-90,-120,95);
+grass.add(blade);
+
+var geometry = new THREE.BoxGeometry(5, 30, 5);
+var blade2 = new THREE.Mesh(geometry, greenMaterial);
+blade2.position.set(-100,-120,100);
+grass.add(blade2);
+
+var geometry = new THREE.BoxGeometry(5, 10, 5);
+var blade3 = new THREE.Mesh(geometry, greenMaterial);
+blade3.position.set(-95,-130,110);
+grass.add(blade3);
+
+var geometry = new THREE.BoxGeometry(5, 25, 5);
+var blade4 = new THREE.Mesh(geometry, greenMaterial);
+blade4.position.set(-93,-130,105);
+grass.add(blade4);
+
+//Ground
+var groundGeometry = new THREE.CylinderGeometry( 210, 230, 20, 20 );
+var groundMaterial = new THREE.MeshLambertMaterial( {color: 0x6f3c11} );
+var ground = new THREE.Mesh( groundGeometry, groundMaterial );
+ground.position.set(0,-145,0);
+grass.add(ground );
+
 
 
 
 //Camera
-var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
 camera.position.y = 160;
 camera.position.z = 400;
 scene.add(camera);
-camera.lookAt(cube.position);
+camera.lookAt(grass.position);
 
 var orbit = new THREE.OrbitControls( camera, renderer.domElement );
 orbit.enableZoom = false;
 // //Rotation factor
-// var clock = new THREE.Clock;
+var clock = new THREE.Clock;
 
 function render() {
   renderer.render(scene, camera);
-  // zebra.rotation.y -= clock.getDelta();
+  zebra.rotation.y -= clock.getDelta();
   requestAnimationFrame(render);
 }
 
