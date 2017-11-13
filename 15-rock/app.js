ac92857d51a15fc95ffe94ab41c8bb993d423916
alribo <30950535+alribo@users.noreply.github.com>
2017-11-13 23:50:49 +0000

    Rolling rock

--- 15-rock/app.js
+++ 15-rock/app.js
@@ -5,62 +5,73 @@ var renderer = new THREE.WebGLRenderer({
   antialias: true
 });
 renderer.setSize(width, height);
-renderer.setClearColor(0x47B2B2);
 document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene;
+scene.background = new THREE.Color( 0xcce0ff );
+scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 );
 
 //Light
-var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 300, 200);
-scene.add(pointLight);
+var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+light.position.set(0, 1000, 0);
+scene.add(light);
 
+var lightPosition4D = new THREE.Vector4();
+lightPosition4D.x = light.position.x;
+lightPosition4D.y = light.position.y;
+lightPosition4D.z = light.position.z;
+lightPosition4D.w = 0.001;
 
 
 
 /* ***** ROCKS ***** */
 var material = new THREE.MeshLambertMaterial({
-  color: 0x745848
+  color: 0x745848,
+  emissive: 0x222222
 });
 
 var rocks;
-for (i=1;i<10;i++){
+var rock;
+// for (i=1;i<3;i++){
 	rocks = new THREE.Group();
 	scene.add(rocks);
-	rocks.name = "rocks" + i;
-	rocks.position.set(Math.round(Math.random()*100*i),Math.round(Math.random()*100*i),Math.round(Math.random()*100*i));
+	rocks.name = "rocks";
+	rocks.position.set(Math.round(Math.random()*100),2000,0);
 	console.log("rocks", rocks);
 
 	//Rock
 	for (index=1;index<10;index++){
-		var geometry = new THREE.DodecahedronGeometry(50,0);
+		var geometry = new THREE.DodecahedronGeometry(300,0);
 		var rock = new THREE.Mesh(geometry, material);
-		rock.name = "rock" + i;
-		rock.position.set(Math.round(Math.random()*100),Math.round(Math.random()*100),Math.round(Math.random()*100));
+		rock.name = "rock" + index;
+		// rock.position.x = Math.round(Math.random*100);
+		rock.position.y = 0;
+		// rock.position.z = Math.round(Math.random*100);
 		rocks.add(rock);
 	}
-}
+
+	var rockShadow = new THREE.ShadowMesh( rock );
+	scene.add( rockShadow );
+
+// }
 
 /* ***** GROUND ***** */
-var geometry = new THREE.PlaneGeometry( 1000, 500, 32 );
-var material = new THREE.MeshLambertMaterial( {color: 0x808080, side: THREE.DoubleSide} );
-var ground = new THREE.Mesh( geometry, material );
-ground.rotation.x = 90*(Math.PI/180);
-ground.position.set(0,-100,0);
-scene.add( ground );
+var groundGeometry = new THREE.BoxGeometry( 10000, 0.01, 10000 );
+var groundMaterial = new THREE.MeshLambertMaterial( { color: 'rgb(0,130,0)' } );
+var groundMesh = new THREE.Mesh( groundGeometry, groundMaterial );
+groundMesh.position.y = 0.0;
+scene.add( groundMesh );
+groundMesh.material.color.setHex( 0x008200 );
+
+var normalVector = new THREE.Vector3( 0, 1, 0 );
+var planeConstant = 0.01;
+var ground = new THREE.Plane( normalVector, planeConstant );
 
 //When rocks touch plane, bounce, break in smaller pieces (between 2 to 5), and roll
-
-
-rocks.rotation.y = 45*(Math.PI/180);
-
-
-
-
 
 //Camera
 var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
 camera.position.y = 160;
-camera.position.z = 400;
+camera.position.z = 2000;
 scene.add(camera);
 camera.lookAt(rocks.position);
 
@@ -70,9 +81,24 @@ orbit.enableZoom = false;
 var clock = new THREE.Clock;
 
 function render() {
-  renderer.render(scene, camera);
-  // rocks.rotation.y -= clock.getDelta();
-  requestAnimationFrame(render);
+  	requestAnimationFrame(render);
+  	var frametime = clock.getDelta();
+  	// rocks.rotation.z -= frametime;
+  	rocks.position.y -= frametime *2000;
+  	if(rocks.position.y  < 250){
+  		rocks.position.y += frametime *2000;
+
+  		rocks.rotation.x += frametime*5;
+  		rocks.position.z += frametime *2000;
+  		if(rocks.position.z > 10000) {
+  			rocks.position.z -= frametime *2000;
+  		}
+  	}
+  	if(rocks.position.y  > 2000){
+  		rocks.position.y -= frametime *2000;
+  	}
+  	rockShadow.update( ground, lightPosition4D );
+  	renderer.render(scene, camera);
 }
 
 render();
